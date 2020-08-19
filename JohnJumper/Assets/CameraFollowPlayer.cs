3bd9536639456204c14504beb87f4c4ceed0af73
William_laptop <williamngoc93@Hotmail.com>
2020-08-19 14:26:01 -0700

    Walls, movement, and camera basics ... Also .gitignore ignores the library files

---
+++ JohnJumper/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [Range(0f,2.5f)]
+    public float CameraYOffset = 0f;
+    private Vector3 vect;
+    public GameObject player;
+    private Transform camTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vect = new Vector3(0, CameraYOffset, 0);
+        camTransform = GetComponent<Transform>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Follow the player
+        camTransform.position = new Vector3(player.transform.position.x, player.transform.position.y,
+            camTransform.position.z) + vect;
+    }
+}
--- JohnJumper/Assets/scripts/playerMovement.cs
+++ JohnJumper/Assets/scripts/playerMovement.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public GameObject playerObject;
     public float speed = 5;
+    private Rigidbody2D rb;
     private float x_axis = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,7 +20,7 @@ public class playerMovement : MonoBehaviour
     {
         if (Input.GetButton("Jump"))
         {
-            Debug.Log("Hello World!");
+            UnityEngine.Debug.Log("Hello World!");
         }
 
         x_axis = Input.GetAxisRaw("Horizontal"); // Returns -1 if moving left, 0 is not moving, 1 if moving right
@@ -31,10 +32,12 @@ public class playerMovement : MonoBehaviour
         switch (x_axis)
         {
             case 1:
-                playerObject.GetComponent<Position> = playerObject.GetComponent<Position> + speed * Time.deltaTime;
+                UnityEngine.Debug.Log("Moving right!");
+                rb.AddForce(new Vector3(speed * Time.deltaTime,0,0), ForceMode2D.Impulse);
                 break;
             case -1:
-                playerObject.GetComponent<Position> = playerObject.GetComponent<Position> - speed * Time.deltaTime;
+                UnityEngine.Debug.Log("Moving left!");
+                rb.AddForce(new Vector3(-speed * Time.deltaTime, 0,0), ForceMode2D.Impulse);
                 break;
         }
     }
