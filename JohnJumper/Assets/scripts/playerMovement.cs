b05175d2bc0fa3d473bbeb26c3775b99c16326ce
William_laptop <williamngoc93@Hotmail.com>
2020-08-19 15:16:20 -0700

    Cool character

--- JohnJumper/Assets/scripts/playerMovement.cs
+++ JohnJumper/Assets/scripts/playerMovement.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public Sprite wallhang;
+    public Sprite midair;
     public float speed = 5;
+    public bool isFacingRight = true;
+
+    private bool isMidAir = false;
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
     private float x_axis = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -39,6 +48,46 @@ public class playerMovement : MonoBehaviour
                 UnityEngine.Debug.Log("Moving left!");
                 rb.AddForce(new Vector3(-speed * Time.deltaTime, 0,0), ForceMode2D.Impulse);
                 break;
+        }
+    }
+
+
+    // Called whenever a collider begins colliding with an object
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //UnityEngine.Debug.Log(collision.collider.name);
+        if (collision.collider.tag == "Wall")
+        {
+            sr.sprite = wallhang;
+            if (collision.collider.name == "LeftWall")
+            {
+                isFacingRight = true;
+                sr.flipX = false;
+            }   else
+            {
+                isFacingRight = false;
+                sr.flipX = true;
+            }
+        }
+    }
+
+
+    // Called whenever a collider stops colliding with an object
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Wall")
+        {
+            sr.sprite = midair;
+            //if (collision.collider.name == "LeftWall")
+            //{
+            //    isFacingRight = true;
+            //    sr.flipX = false;
+            //}
+            //else
+            //{
+            //    isFacingRight = false;
+            //    sr.flipX = true;
+            //}
         }
     }
 }
