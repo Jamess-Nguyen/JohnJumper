d2d90ab649b204d527c9d1d50731f85ab65a8d76
William_laptop <williamngoc93@Hotmail.com>
2020-09-01 15:52:43 -0700

    Did more looping.

--- JohnJumper/Assets/scripts/LoopAround.cs
+++ JohnJumper/Assets/scripts/LoopAround.cs
@@ -6,6 +6,8 @@ public class LoopAround : MonoBehaviour
 {
     public float yPositionStart = -40f;
     public float yPositionEnd = 40f;
+
+    private int numLoops = 0;
     private Transform playerTransform;
 
     // Start is called before the first frame update
@@ -19,7 +21,14 @@ public class LoopAround : MonoBehaviour
         if (playerTransform.position.y >= yPositionEnd)
         {
             Debug.Log("LOOP!");
-            playerTransform.position = new Vector3(playerTransform.position.x, yPositionStart);
+            numLoops++;
+            playerTransform.position = new Vector3(playerTransform.position.x, yPositionStart+10f);
+        }
+        if (numLoops > 0 && playerTransform.position.y <= yPositionStart)
+        {
+            Debug.Log("UNDO LOOP!");
+            numLoops--;
+            playerTransform.position = new Vector3(playerTransform.position.x, yPositionEnd-10f);
         }
     }
 }
